0c520a99e422480fa6824e46b8ca8a8d91b59745
Manuel <25lopezm25@gmail.com>
2022-05-14 19:58:28 -0600

    cambios en la escena

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager sharedInstance;
+
+    private void Awake()
+    {
+        if(sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        Application.targetFrameRate = 60;
+    }
+}
